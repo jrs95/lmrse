0b2c4b3ce149a2ef567ea640796cc6b741bed061
James Staley <jrs95@users.noreply.github.com>
2018-03-26 15:01:00 +0100

    Update lmrse.R

--- R/lmrse.R
+++ R/lmrse.R
@@ -50,7 +50,7 @@ lmrse <- function(formula, cluster, data=NULL){
   if(any(miss)==T){
     b_nc <- data.frame()
     for(i in 1:ncol(y_nc)){
-      b_nc <- rbind(b_nc, t(fastLm(y_nc~x-1)$coef))
+      b_nc <- rbind(b_nc, t(fastLm(y_nc[i,]~x-1)$coef))
     }
     b_nc <- as.matrix(b_nc)
   }
