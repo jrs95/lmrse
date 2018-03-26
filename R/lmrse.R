f622104a5737a7df03e7f69bd4399b4646b53ed9
James Staley <jrs95@users.noreply.github.com>
2018-03-26 14:45:10 +0100

    Update lmrse.R

--- R/lmrse.R
+++ R/lmrse.R
@@ -29,7 +29,7 @@
 lmrse <- function(formula, cluster, data=NULL){
   
   # Formulae
-  if(!is.null(data)){mf <- model.frame(formula, data)}else{mf <- model.frame(formula)}
+  if(!is.null(data)){mf <- model.frame(formula, data, na.action=NULL)}else{mf <- model.frame(formula, na.action=NULL)}
   y <- model.response(mf, "numeric")
   if(!is.null(data)){x <- model.matrix(formula, data)}else{x <- model.matrix(formula, mf)}
   
